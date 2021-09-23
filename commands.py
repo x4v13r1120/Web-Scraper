bc96baa744f679124358ba59a0970dd218873c2f
xavier1120 <xaviernaranjo1130@gmail.com>
2021-09-23 19:58:30 -0400

    format touch up

--- commands.py
+++ commands.py
@@ -1,6 +1,4 @@
-
-
-class command:
+class commands:
     api_key = "F6KD4YVEAVT9P6M2U3TPCGZCXT4PRQG4U9"
     website = "https://api.etherscan.io/api?"
     module = [
@@ -15,126 +13,184 @@ class command:
         "token"
     ]
 
-    @staticmethod
-    def __init__(api_key, website, module):
+### DEFAULT CONSTRUCTOR ###
+    def __init__(self, api_key, website, module):
         api_key = api_key
         website = website
         module = module
 
+### START OF ACCOUNT MODULE COMMANDS ###
     # Get Ether Balance for a Single Address
     @staticmethod
-    def command0(api_key, website, module, action, address, tag):
-        apikey = command.api_key
-        website = command.website
-        module = command.module[0]  # account module
-        action = action
-        address = address
-        tag = tag
-        return f"{website}&module={module}&action={action}&address={address}&tag={tag}&apikey={api_key}"
+    def accountCommand0(address):
+        action = "balance"
+        tag = "latest"
+        return f"{commands.website}&module={commands.module[0]}" \
+               f"&action={action}&address={address}&tag={tag}&apikey={commands.api_key}"
 
     # Get Ether Balance for Multiple Addresses in a Single Call
     @staticmethod
-    def command1(api_key, website, module, action, address, tag):
-        website = command.website
-        apikey = command.api_key
-        action = action
-        module = command.module[0]
-        address = address
-        tag = tag
-        return f"{website}&module={module}&action={action}&address={address}&tag={tag}&apikey={api_key}"
+    def accountCommand1(address):
+        action = "balancemulti"
+        tag = "latest"
+        return f"{commands.website}&module={commands.module[0]}" \
+               f"&action={action}&address={address}&tag={tag}&apikey={commands.api_key}"
 
     # Get a list of 'Normal' Transactions By Address
     @staticmethod
-    def command2(api_key, website, module, action, address):
-        website = command.website
-        apikey = command.api_key
-        action = action
-        module = command.module[0]
-        address = address
-        startBlock = 0
-        endBlock = 99999999
+    def accountCommand2(address):
+        action = "txlist"
+        start_block = 0
+        end_block = 99999999
         page = 1
         offset = 10
         sort = "asc"
-        return f"{website}&module={module}&action={action}&address={address}" \
-               f"&startblock={startBlock}&endblock={endBlock}&page={page}" \
-               f"&offset={offset}&sort={sort}&apikey={api_key}"
+        return f"{commands.website}&module={commands.module[0]}&action={action}&address={address}" \
+               f"&startblock={start_block}&endblock={end_block}&page={page}" \
+               f"&offset={offset}&sort={sort}&apikey={commands.api_key}"
 
-    @staticmethod
     # get a list of 'internal transactions by address'
-    def command3(api_key, website, module, action, address):
-        website = command.website
-        apikey = command.api_key
-        module = command.module[0]
-        action = action
-        address = address
-        startBlock = 0
-        endBlock = 99999999
+    @staticmethod
+    def accountCommand3(address):
+        action = "txlistinternal"
+        start_block = 0
+        end_block = 99999999
         page = 1
         offset = 10
         sort = "asc"
-        return f"{website}&module={module}&action={action}&address={address}" \
-               f"&startblock={startBlock}&endblock={endBlock}&page={page}" \
-               f"&offset={offset}&sort={sort}&apikey={api_key}"
+        return f"{commands.website}&module={commands.module[0]}&action={action}&address={address}" \
+               f"&startblock={start_block}&endblock={end_block}&page={page}" \
+               f"&offset={offset}&sort={sort}&apikey={commands.api_key}"
 
-    @staticmethod
     # Get 'Internal Transactions' by Transaction Hash
-    def command4(apikey, website, module, action, txhash):
-        website = website
-        apikey = command.api_key
-        action = action
-        module = command.module[0]
-        txhash = txhash
-        return  f"{website}&module={module}&action={action}&txhash={txhash}&apikey={apikey}"
-
     @staticmethod
+    def accountCommand4(txhash):
+        action = "txlistinternal"
+        return f"{commands.website}&module={commands.module[0]}" \
+               f"&action={action}&txhash={txhash}&apikey={commands.api_key}"
+
     # get "internal transactions" by block range
-    def command5(apikey, website, module, action):
-        website = command.website
-        module = command.module[0]
-        apikey = command.api_key
-        action = action
+    @staticmethod
+    def accountCommand5():
+        action = "txlistinternal"
         startBlock = 0
         endBlock = 99999999
         page = 1
         offset = 10
         sort = "asc"
-        return f"{website}&module={module}&action={action}&startblock={startBlock}&endblock={endBlock}&page={page}" \
-               f"&offset={offset}&sort={sort}&apikey={apikey}"
+        return f"{commands.website}&module={commands.module[0]}&action={action}" \
+               f"&startblock={startBlock}&endblock={endBlock}&page={page}" \
+               f"&offset={offset}&sort={sort}&apikey={commands.api_key}"
 
-
-"""
     # Get a list of 'ERC20 - Token Transfer Events' by Address
-    def command7(command7):
-        command7.website = website
-        command7.apikey = api_key
-        command7.action = "tokentx"
-        command7.module = module_account
-        command7.address = ""
-        command7.page = 1
-        command7.offset = 100
-        command7.sort = "asc"
-        command7.contractaddress = ""
+    @staticmethod
+    def accountCommand6(address):
+        action = "tokentx"
+        page = 1
+        offset = 100
+        sort = "asc"
+        contractaddress = "0x9f8f72aa9304c8b593d555f12ef6589cc3a579a2"
+        return f"{commands.website}&module={commands.module[0]}&action={action}" \
+               f"&=contractaddress={contractaddress}&page={page}" \
+               f"&offset={offset}&sort={sort}&apikey={commands.api_key}"
 
     # Get a list of 'ERC721 - Token Transfer Events' by Address
-    def command8(command8):
-        command8.website = website
-        command8.module = module_account
-        command8.action = "tokennfttx"
-        command8.contractAddress = ""
-        command8.address = ""
-        command8.page = 1
-        command8.offset = 100
-        command8.sort = "asc"
-        command8.apikey = api_key
+    @staticmethod
+    def accountCommand7(address):
+        action = "tokennfttx"
+        contractaddress = "0x9f8f72aa9304c8b593d555f12ef6589cc3a579a2"
+        page = 1
+        offset = 100
+        sort = "asc"
+        return f"{commands.website}&module={commands.module[0]}&action={action}" \
+               f"&=contractaddress={contractaddress}&page={page}" \
+               f"&offset={offset}&sort={sort}&apikey={commands.api_key}"
 
     # Get list of Blocks Mined by Address
-   def command9(command9):
-       command9.website = website
-       command9.module = module_account
-       command9.action = "getminedblocks"
-       command9.blocktype = "blocks"
-       command9.address = ""
-       command9.page = 1
-        command9.offset = 100
-"""
+    @staticmethod
+    def accountCommand8(address):
+        action = "getminedblocks"
+        blocktype = "blocks"
+        page = 1
+        offset = 100
+        return f"{commands.website}&module={commands.module[0]}&action={action}&=blocktype={blocktype}&page={page}" \
+               f"&offset={offset}&apikey={commands.api_key}"
+
+### START OF CONTRACT MODULE COMMANDS ###
+
+# Get Contract ABI for Verified Contract Source Code
+    @staticmethod
+    def contractCommand0(address):
+        action = "getabi"
+        return f"{commands.website}&module={commands.module[1]}" \
+               f"&action={action}&address={address}&apikey={commands.api_key}"
+
+# Get Contract Source Code for Verified Contract Source Codes
+    @staticmethod
+    def contractCommand1(address):
+        action = "getsourcecode"
+        return f"{commands.website}&module={commands.module[1]}" \
+               f"&action={action}&address={address}&apikey={commands.api_key}"
+
+### START OF TRANSACTIONS MODULE COMMANDS ###
+
+#Check Contract Execution Status
+    @staticmethod
+    def transactionCommand0(txhash):
+        action = "getstatus"
+        return f"{commands.website}&module={commands.module[2]}" \
+               f"&action={action}&txhash={txhash}&apikey={commands.api_key}"
+
+# Check Transaction Receipt Status
+    @staticmethod
+    def transactionCommand1(txhash):
+        action = "gettxreceiptstatus"
+        txhash = txhash
+        return f"{commands.website}&module={commands.module[2]}" \
+               f"&action={action}&txhash={txhash}&apikey={commands.api_key}"
+
+### START OF BLOCK MODULE COMMANDS ###
+
+# Get Block And Uncle Rewards by BlockNo
+    @staticmethod
+    def blockCommand0(blockno):
+        action = "getblockreward"
+        return f"{commands.website}&module={commands.module[3]}" \
+               f"&action={action}&blockno={blocko}&apikey={commands.api_key}"
+
+# Get Estimated Block Countdown Time by BlockNo
+    @staticmethod
+    def blockCommand1(blockno):
+        action = "getblockcountdown"
+        return f"{commands.website}&module={commands.module[3]}" \
+               f"&action={action}&blockno={blockno}&apikey={commands.api_key}"
+
+# Get Block Number by Timestamp
+    @staticmethod
+    def blockCommand2(timestamp):
+        action = "getblocknobytime"
+        closest = "before"
+        return f"{commands.website}&module={commands.module[3]}&action={action}" \
+               f"&timestamp={timestamp}&closest={closest}&apikey={commands.api_key}"
+
+### START OF LOGS MODULE COMMANDS ###
+
+# Sample Log API Queries
+
+    # Get Event Logs from block number 379224 to 'latest' Block,
+    # where log address = 0x33990122638b9132ca29c723bdf037f1a891a70c
+    # and topic[0] = 0xf63780e752c6a54a94fc52715dbc5518a3b4c3c2833d301a204226548a2a8545
+    @staticmethod
+    def logCommand0(address):
+        action = "getLogs"
+        fromBlock = 379224
+        toBlock = "latest"
+        address = address
+        topic0 = "0xf63780e752c6a54a94fc52715dbc5518a3b4c3c2833d301a204226548a2a8545" #test hash
+        return f"{commands.website}&module={commands.module[4]}&action={action}&fromBlock={fromBlock}" \
+               f"&toBlock={toBlock}&address={address}&topic0={topic0}&apikey={commands.api_key}"
+
+### START OF GETH/PARITY/PROXY MODULE COMMANDS ###
+### START OF TOKENS MODULE COMMANDS ###
+### START OF GAS TRACKER MODULE COMMANDS ###
+### START OF STATS MODULE COMMANDS ###
--- extract_data.py
+++ extract_data.py
@@ -1,42 +1,44 @@
 import json
 import requests
-from commands import command
+from commands import commands
 
-
-action = str(input("Please enter action you'd like to perform."))  # balance, balancemulti, txlist, txlistinternal,
-# tokentx, tokennfttx, getminedblocks, balancehistory,
 address = str(input("Please enter Ether Address."))  # 0xde0b295669a9fd93d5f28d9ec85e40f4cb697bae
-tag = str(input("Please enter tag."))  # latest
 
-commandList = {
-    0: command.command0(command.api_key, command.module[0], command.website,
-                        action, address, tag),  # Get Ether
-    # Balance for a Single Address
+accountCommandsList = {
+    "etherBalanceSingle": commands.command0(address),  # Get Ether Balance for a Single Address
 
-    1: command.command1(command.api_key, command.module[0], command.website,
-                        action, address, tag),  # Get Ether
-    # Balance for Multiple Addresses in a Single Call
+    "etherBalanceMultiple": commands.command1(address),  # Get Ether Balance for Multiple Addresses in a Single Call
 
-    2: command.command2(command.api_key, command.website, command.module[0],
-                        action, address),  # Get a list of
-    # 'Normal' Transactions By Address
+    "listNormalTransactions": commands.command2(address),  # Get a list of'Normal' Transactions By Address
 
-    3: command.command3(command.api_key, command.website, command.module[0],
-                        action, address), # get a list of 'internal transactions by address'
+    "listInternalTransactions": commands.command3(address),  # get a list of 'internal transactions by address'
 
-    4: command.command4(command.api_key, command.website, command.module[0],action,
-                        txhash= 0x40eb908387324f2b575b4879cd9d7188f69c8fc9d87c901b9e2daaea4b442170),
+    "txhashInternalTransactions": commands.command4(txhash = 0x40eb908387324f2b575b4879cd9d7188f69c8fc9d87c901b9e2daaea4b442170),
     # Get 'Internal Transactions' by Transaction Hash
 
-    5:  command.command5(command.api_key, command.website, command.module[0], action), # get
-    # "internal transactions" by block range
+    "blockRangeInternalTx": commands.command5(),  # get "internal transactions" by block range
+
+    "listErc20Transfer": commands.command6(address), # Get a list of 'ERC20 - Token Transfer Events' by Address
+
+    "listErc721Transfer": commands.command7(address), # Get a list of 'ERC721 - Token Transfer Events' by Address
+
+    "listBlocksMined": commands.command8(address), # Get list of Blocks Mined by Address
 
 }
+# contractCommandsList{ }
+
+# module_choice = input("What module do u wish to utilize:"}
+
+command_choice = input(f"which command from the {module_choice} would you like to use:\n1.{list(accountCommandsList.keys())[0]}\n"
+      f"2.{list(accountCommandsList.keys())[1]}\n3.{list(accountCommandsList.keys())[2]}\n"
+      f"4.{list(accountCommandsList.keys())[3]}\n5.{list(accountCommandsList.keys())[4]}\n"
+      f"6.{list(accountCommandsList.keys())[5]}\n7.{list(accountCommandsList.keys())[6]}\n"
+      f"8.{list(accountCommandsList.keys())[7]}\n")
 
 
 def extract():
     # make request using command
-    response = requests.get(commandList[2])
+    response = requests.get(accountCommandsList[command_choice - 1])
     address_content = response.json()
     result = address_content.get("result")
     # dump data
