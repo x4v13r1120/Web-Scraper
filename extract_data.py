6fe02fd23538a7e9d7270a0a95aa334420aa4adb
xavier1120 <xaviernaranjo1130@gmail.com>
2021-09-23 22:31:15 -0400

    commands for account contract and transactions should work using correct user input . still ne input error handling.

--- extract_data.py
+++ extract_data.py
@@ -2,45 +2,96 @@ import json
 import requests
 from commands import commands
 
-address = str(input("Please enter Ether Address."))  # 0xde0b295669a9fd93d5f28d9ec85e40f4cb697bae
+module_choice = int(input(f"What module do u wish to utilize:\n1.{commands.module[0]}\n2.{commands.module[1]}\n"
+                      f"3.{commands.module[2]}\n4.{commands.module[3]}\n5.{commands.module[4]}\n"
+                      f"6.{commands.module[5]}\n7.{commands.module[6]}\n8.{commands.module[7]}\n"
+                      f"9.{commands.module[8]}\n"))
+
+address = str(input("Please enter Ether Address."))  # address for testing = 0xde0b295669a9fd93d5f28d9ec85e40f4cb697bae
 
 accountCommandsList = {
-    "etherBalanceSingle": commands.command0(address),  # Get Ether Balance for a Single Address
+    "etherBalanceSingle": commands.accountCommand0(address),  # Get Ether Balance for a Single Address
 
-    "etherBalanceMultiple": commands.command1(address),  # Get Ether Balance for Multiple Addresses in a Single Call
+    "etherBalanceMultiple": commands.accountCommand1(address),  # Get Ether Balance for Multiple Addresses in a Single Call
 
-    "listNormalTransactions": commands.command2(address),  # Get a list of'Normal' Transactions By Address
+    "listNormalTransactions": commands.accountCommand2(address),  # Get a list of'Normal' Transactions By Address
 
-    "listInternalTransactions": commands.command3(address),  # get a list of 'internal transactions by address'
+    "listInternalTransactions": commands.accountCommand3(address),  # get a list of 'internal transactions by address'
 
-    "txhashInternalTransactions": commands.command4(txhash = 0x40eb908387324f2b575b4879cd9d7188f69c8fc9d87c901b9e2daaea4b442170),
+    "txhashInternalTransactions": commands.accountCommand4(txhash = 0x40eb908387324f2b575b4879cd9d7188f69c8fc9d87c901b9e2daaea4b442170),
     # Get 'Internal Transactions' by Transaction Hash
 
-    "blockRangeInternalTx": commands.command5(),  # get "internal transactions" by block range
+    "blockRangeInternalTx": commands.accountCommand5(),  # get "internal transactions" by block range
 
-    "listErc20Transfer": commands.command6(address), # Get a list of 'ERC20 - Token Transfer Events' by Address
+    "listErc20Transfer": commands.accountCommand6(address), # Get a list of 'ERC20 - Token Transfer Events' by Address
 
-    "listErc721Transfer": commands.command7(address), # Get a list of 'ERC721 - Token Transfer Events' by Address
+    "listErc721Transfer": commands.accountCommand7(address), # Get a list of 'ERC721 - Token Transfer Events' by Address
 
-    "listBlocksMined": commands.command8(address), # Get list of Blocks Mined by Address
+    "listBlocksMined": commands.accountCommand8(address), # Get list of Blocks Mined by Address
 
 }
-# contractCommandsList{ }
+contractCommandsList = {
+    "getAbiContract": commands.contractCommand0(address), # Get Contract ABI for Verified Contract Source Code
 
-# module_choice = input("What module do u wish to utilize:"}
+    "getContractSource": commands.contractCommand1(address) # Get Contract Source Code for Verified
+    # Contract Source Codes
+}
+transactionCommandsList = {
+    "checkContractStatus": commands.transactionCommand0(
+        txhash = 0x15f8e5ea1079d9a0bb04a4c58ae5fe7654b5b2b4463375ff7ffb490aa0032f3a), #Check Contract Execution Status
+    "checkTransactionStatus":commands.transactionCommand1(
+        txhash = 0x15f8e5ea1079d9a0bb04a4c58ae5fe7654b5b2b4463375ff7ffb490aa0032f3a) # Check Transaction Receipt Status
+}
 
-command_choice = input(f"which command from the {module_choice} would you like to use:\n1.{list(accountCommandsList.keys())[0]}\n"
-      f"2.{list(accountCommandsList.keys())[1]}\n3.{list(accountCommandsList.keys())[2]}\n"
-      f"4.{list(accountCommandsList.keys())[3]}\n5.{list(accountCommandsList.keys())[4]}\n"
-      f"6.{list(accountCommandsList.keys())[5]}\n7.{list(accountCommandsList.keys())[6]}\n"
-      f"8.{list(accountCommandsList.keys())[7]}\n")
+module_command_list = [
+    accountCommandsList,
+    contractCommandsList,
+    transactionCommandsList
+]
 
+passthrough = ""
+
+if module_choice == 1:
+    command_choice = int(input(f"which command you like to use:\n1.{list(accountCommandsList.keys())[0]}\n"
+          f"2.{list(accountCommandsList.keys())[1]}\n3.{list(accountCommandsList.keys())[2]}\n"
+          f"4.{list(accountCommandsList.keys())[3]}\n5.{list(accountCommandsList.keys())[4]}\n"
+          f"6.{list(accountCommandsList.keys())[5]}\n7.{list(accountCommandsList.keys())[6]}\n"
+          f"8.{list(accountCommandsList.keys())[7]}\n"))
+    coversion_dict = {
+        0:"etherBalanceSingle",
+        1:"etherBalanceMultiple",
+        2:"listNormalTransactions",
+        3:"listInternalTransactions",
+        4:"txhashInternalTransactions",
+        5:"blockRangeInternalTx",
+        6:"listErc20Transfer",
+        7:"listErc721Transfer",
+        8:"listBlocksMined"
+    }
+    passthrough = module_command_list[module_choice-1][coversion_dict[command_choice-1]]
+elif module_choice == 2:
+    command_choice = int(input(f"which command you like to use:\n1.{list(contractCommandsList.keys())[0]}\n"
+                               f"2.{list(contractCommandsList.keys())[1]}\n"))
+    coversion_dict = {
+        0: "getAbiContract",
+        1: "getContractSource",
+    }
+    passthrough = module_command_list[module_choice - 1][coversion_dict[command_choice - 1]]
+elif module_choice == 3:
+    command_choice = int(input(f"which command you like to use:\n1.{list(transactionCommandsList.keys())[0]}\n"
+                               f"2.{list(transactionCommandsList.keys())[1]}\n"))
+    coversion_dict = {
+        0: "checkContractStatus",
+        1: "checkTransactionStatus",
+    }
+    passthrough = module_command_list[module_choice - 1][coversion_dict[command_choice - 1]]
 
 def extract():
     # make request using command
-    response = requests.get(accountCommandsList[command_choice - 1])
+    response = requests.get(passthrough)
     address_content = response.json()
     result = address_content.get("result")
+
     # dump data
     with open("data/jsondata.json", "w") as outfile:
         json.dump(result, outfile)
@@ -62,3 +113,6 @@ def extract():
                 if not line.strip().startswith("//"):
                     if not comment:
                         outfile.write(f"{line}\n")
+                        print("Data Extracted & Cleaned!!! Ready for transformation!!!")
+
+
