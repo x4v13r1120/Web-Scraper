c23144568ea9babb3d087f6cfc74a72a15aac3fb
xavier1120 <xaviernaranjo1130@gmail.com>
2021-11-10 12:53:03 -0500

    doesnt recognize data in sql statement for ?

--- ETL/load_data.py
+++ ETL/load_data.py
@@ -18,21 +18,23 @@ def load(command_choice):
     rawdata = {}
     with open("data/cleaned_data.json") as infile:
         rawdata = json.load(infile)
-    for keys in rawdata:
         ethdata = []
-        for value in keys.values():
-            ethdata.append(value)
-    for value in keys.keys():
-        table.append(value)
-    print(SQLStatement().buildSqlStatement(command_choice))
-    # conn.execute(SQLStatement().buildSqlStatement())
+        for keys in rawdata:
+            for value in keys.values():
+                ethdata.append(value)
+        for value in keys.keys():
+            table.append(value)
+        sql = SQLStatement().buildSqlStatement(command_choice)
+        print(sql)
+        for keys in rawdata:
+            conn.execute(sql)
     conn.commit()
     conn.close()
     print("Data successfully loaded into database!!!")
 
 
 class SQLStatement:
-    PREFIX = 'INSERT INTO'
+    PREFIX = "INSERT INTO"
     AccountModuleTableNames = [
         'Account',
         'TransactionsByAddress',
@@ -43,26 +45,27 @@ class SQLStatement:
         'BlocksMinedByAddress'
 
     ]
-    SUFFIX = 'VALUES'
+    SUFFIX = "VALUES"
 
     def __init__(self):
         self.sqlString = ''
 
     def buildSqlStatement(self, x):
         self.sqlString = self.PREFIX + ' ' + get_choice(x) + \
-                         '(' + ','.join(val if val else '' for val in table) + ')' + self.SUFFIX + '(' \
-                         + ','.join('?' for val in table) + ')'
+                         '(' + ','.join(f"'{val}'" if val else '' for val in table) + ')' \
+                         + self.SUFFIX + '(' \
+                         + ','.join('?' for val in table) + ');'
         return self.sqlString
 
 
 def get_choice(x):
-    switcher = {
-        1 or 2: SQLStatement.AccountModuleTableNames[0],
-        4 or 6: SQLStatement.AccountModuleTableNames[2],
-        3: SQLStatement.AccountModuleTableNames[1],
-        5: SQLStatement.AccountModuleTableNames[3],
-        7: SQLStatement.AccountModuleTableNames[4],
-        8: SQLStatement.AccountModuleTableNames[5],
-        9: SQLStatement.AccountModuleTableNames[6]
+    switcher1 = {
+        1: SQLStatement.AccountModuleTableNames[0],
+        3 or 5: SQLStatement.AccountModuleTableNames[2],
+        2: SQLStatement.AccountModuleTableNames[1],
+        4: SQLStatement.AccountModuleTableNames[3],
+        6: SQLStatement.AccountModuleTableNames[4],
+        7: SQLStatement.AccountModuleTableNames[5],
+        8: SQLStatement.AccountModuleTableNames[6]
     }
-    return switcher.get(x, "invalid")
+    return switcher1.get(x, "invalid")
